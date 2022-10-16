ac9655bc6862a5e3ab58ce3a14cba407dda91540
knoellle <konrad@familie-noelle.de>
2022-10-17 00:06:09 +0200

    Switch to cpal

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,53 @@
-use pipewire::{
-    properties,
-    spa::Direction,
-    stream::{ListenerBuilderT, StreamFlags},
-    Context, MainLoop,
-};
+use anyhow::Ok;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use ringbuf::HeapRb;
 
 fn main() -> anyhow::Result<()> {
-    let mainloop = MainLoop::new()?;
-    let mut stream = pipewire::stream::Stream::<()>::simple(
-        &mainloop,
-        "audio-test",
-        properties! {
-            *pipewire::keys::MEDIA_TYPE => "Audio",
-            *pipewire::keys::MEDIA_CLASS => "Audio/Sink",
-            *pipewire::keys::MEDIA_CATEGORY => "Duplex",
-            *pipewire::keys::MEDIA_ROLE => "DSP",
-        },
+    let host = cpal::host_from_id(
+        cpal::available_hosts()
+            .into_iter()
+            .find(|id| *id == cpal::HostId::Jack)
+            .expect("No jack server found"),
     )
-    .state_changed(|old, new| {
-        println!("State changed: {:?} -> {:?}", old, new);
-    })
-    .process(|_stream, _user_data| {
-        println!("On frame");
-    })
-    .create()?;
+    .expect("Found but failed to talk to jack server");
 
-    stream.connect(
-        Direction::Input,
-        None,
-        StreamFlags::AUTOCONNECT | StreamFlags::MAP_BUFFERS | StreamFlags::RT_PROCESS,
-        &mut [],
-    )?;
+    let input_device = host
+        .default_input_device()
+        .expect("Failed to find input device");
+    let output_device = host
+        .default_output_device()
+        .expect("Failed to find output device");
 
-    mainloop.run();
+    let config: cpal::StreamConfig = input_device.default_input_config()?.into();
+
+    let ring = HeapRb::new(4096);
+    let (mut producer, mut consumer) = ring.split();
+    let handle_input = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+        if data.iter().all(|f| f.abs() < 0.01) {
+            return;
+        }
+        println!("len: {}", data.len());
+        println!("sum: {}", data.iter().sum::<f32>());
+        for &sample in data {
+            producer.push(sample);
+        }
+    };
+    let handle_output = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        for sample in data {
+            *sample = consumer.pop().unwrap_or(0.0);
+        }
+    };
+
+    let input_stream = input_device.build_input_stream(&config, handle_input, err_fn)?;
+    let output_stream = output_device.build_output_stream(&config, handle_output, err_fn)?;
+
+    input_stream.play()?;
+    output_stream.play()?;
+    std::thread::sleep(std::time::Duration::from_secs(300));
 
     Ok(())
+}
+
+fn err_fn(err: cpal::StreamError) {
+    eprintln!("an error occurred on stream: {}", err);
 }
